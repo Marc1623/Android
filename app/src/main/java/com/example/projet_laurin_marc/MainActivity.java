1e0ac6f3aaeaa195d93e56a91875b3da0dedbe5e
Marc Vial <marc.vial@hes-so.ch>
2020-02-27 10:35:52 +0100

    First commit. Only the base.

---
+++ app/src/main/java/com/example/projet_laurin_marc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.projet_laurin_marc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
