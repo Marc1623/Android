5c22dcb37c1b46de6cecdcbb47ea9754db3de531
WeLa <laurin.weber@students.hevs.ch>
2020-03-07 21:13:01 +0100

    UI: Bottom NavigationBar created -> Androidx

---
+++ app/src/main/java/com/example/projet_laurin_marc/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.projet_laurin_marc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
