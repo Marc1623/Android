9d4d424a74e54e851a8cdbbf3374786c62e31d94
Marc Vial <marc.vial@hes-so.ch>
2020-03-08 17:25:08 +0100

    UI Cantons

---
+++ app/src/main/java/com/example/projet_laurin_marc/CommuneActivity.java
@@ -0,0 +1,14 @@
+package com.example.projet_laurin_marc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommuneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commune);
+    }
+}
