6de8bb172868ecfd4decdeaf37b4ebbe176518cd
Marc Vial <marc.vial@hes-so.ch>
2020-03-28 15:12:30 +0100

    Settings in progress

--- app/src/main/java/com/example/projet_laurin_marc/ui/mgmt/SettingsActivity.java
+++ app/src/main/java/com/example/projet_laurin_marc/ui/mgmt/SettingsActivity.java
@@ -10,6 +10,8 @@ public class SettingsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		getFragmentManager().beginTransaction().replace(android.R.id.content,new SettingsFragment()).commit();
+		getFragmentManager().beginTransaction()
+				.replace(android.R.id.content,new SettingsFragment())
+				.commit();
 	}
 }
